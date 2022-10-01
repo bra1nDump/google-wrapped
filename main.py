e9b5c3d04319475119dfb1c2707cd2e4b065795c
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-10-01 01:36:07 -0700

    initial

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import Spark NLP
+from sparknlp.base import *
+from sparknlp.annotator import *
+from sparknlp.pretrained import PretrainedPipeline
+import sparknlp
+
+# Start SparkSession with Spark NLP
+# start() functions has 3 parameters: gpu, m1, and memory
+# sparknlp.start(gpu=True) will start the session with GPU support
+# sparknlp.start(m1=True) will start the session with macOS M1 support
+# sparknlp.start(memory="16G") to change the default driver memory in SparkSession
+spark = sparknlp.start()
+
+# Download a pre-trained pipeline
+pipeline = PretrainedPipeline('explain_document_dl', lang='en')
+
+# Your testing dataset
+text = """
+The Mona Lisa is a 16th century oil painting created by Leonardo.
+It's held at the Louvre in Paris.
+"""
+
+# Annotate your testing dataset
+result = pipeline.annotate(text)
+
+# What's in the pipeline
+list(result.keys())
+Output: ['entities', 'stem', 'checked', 'lemma', 'document',
+'pos', 'token', 'ner', 'embeddings', 'sentence']
+
+# Check the results
+result['entities']
+Output: ['Mona Lisa', 'Leonardo', 'Louvre', 'Paris']
