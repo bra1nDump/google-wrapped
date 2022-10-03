93473ed90b43ddab906b2f337be69627dd72403c
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-10-03 06:30:43 +0000

    Figured out I need to source the script, Added notes Added example history

--- codespaces_setup.sh
+++ codespaces_setup.sh
@@ -1,5 +1,9 @@
-conda activate sparknlp
+pip install -r requirements.txt
+
+sudo apt-get install -yq openjdk-8-jre
 
 # Spark needs 8/11
-alias java=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+export PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/:$PATH"
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# !! For the exports to work run > source codespaces_setup.sh
