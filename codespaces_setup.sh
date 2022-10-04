4675765a8ca7ff50ca38b9cfa7b13375a26a80dd
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-10-04 07:08:08 +0000

    another attempt to fix this shit

--- codespaces_setup.sh
+++ codespaces_setup.sh
@@ -1,10 +1,9 @@
 pip install -r requirements.txt
-pip3 install -r requirements.txt
-
-sudo apt-get install -yq openjdk-8-jre
 
 # Spark needs 8/11
-export PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/:$PATH"
+sudo apt-get install -yq openjdk-8-jre
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+export LOL=lol
 
 # !! For the exports to work run > source codespaces_setup.sh
