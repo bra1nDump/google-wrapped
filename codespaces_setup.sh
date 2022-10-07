f27492682d0e087fb11a45341e952a7952819f57
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-10-07 05:09:14 +0000

    cleaning up before letting people into the house

--- codespaces_setup.sh
+++
@@ -1,9 +0,0 @@
-pip install -r requirements.txt
-
-# Spark needs 8/11
-sudo apt-get install -yq openjdk-8-jre
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-export LOL=lol
-
-# !! For the exports to work run > source codespaces_setup.sh
